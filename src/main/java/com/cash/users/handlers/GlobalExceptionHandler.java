c4ccaedef9a2fe57082b94c7c75e6b4c8bd646b0
Tomás Davison <toomi.davison@gmail.com>
2020-09-30 23:11:01 -0300

    Adjust GlobalExceptionHandler

--- src/main/java/com/cash/users/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/cash/users/handlers/GlobalExceptionHandler.java
@@ -1,4 +1,26 @@
 package com.cash.users.handlers;
 
+import com.cash.users.models.ResponseBody;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Collections;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    private ResponseEntity<Object> missingParameterException(
+            MissingServletRequestParameterException e,
+            WebRequest request) {
+        ResponseBody response = ResponseBody.notOk(Collections.singletonList("Parameter '" + e.getParameterName() + "' is mandatory"));
+        return ResponseEntity.badRequest().body(response);
+    }
 }
